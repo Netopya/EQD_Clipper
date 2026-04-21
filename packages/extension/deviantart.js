190c5d3af4cdc85db72429f8ae85f3c147d6edea
Netopya <netopya12123+github@hotmail.com>
2026-04-20 21:47:41 -0400

    Improve da

--- packages/extension/deviantart.js
+++ packages/extension/deviantart.js
@@ -6,11 +6,31 @@ const MODAL_WAIT_MS = 4000;
 
 const started = Date.now();
 
-const search = setInterval(() => {
-  const downloadButton = document.querySelector('[download]');
+function waitForElement(selector, time = 3000, validation = () => true) {
+  return Promise((resolve, reject) => {
+    const elementLocator = () => document.querySelector(selector);
 
-  if (downloadButton?.href) {
-    clearInterval(search);
+    const search = setInterval(() => {
+      const element = elementLocator();
+  
+      if (element && validation(element)) {
+        clearInterval(search);
+        resolve(element);
+      }
+    }, INTERVAL_MS);
+
+    setTimeout(() => {
+      clearInterval(search);
+      reject();
+    }, timeout);
+  });
+}
+
+
+
+(async() => {
+  try {
+    const downloadButton = waitForElement('[download]', DOWNLOAD_SEARCH_MS, (element) => { !!element.href });
     downloadButton.style.outline = '5px solid purple';
     port.postMessage({
       msg: 'DownloadThis',
@@ -24,39 +44,37 @@ const search = setInterval(() => {
       },
     });
     return;
-  }
-
-  if (Date.now() - started > DOWNLOAD_SEARCH_MS) {
-    clearInterval(search);
+  } catch(ex) {
     openModalFallback();
   }
-}, INTERVAL_MS);
 
-function openModalFallback() {
-  const firstImage = document.querySelector('img[fetchpriority="high"]');
-  if (!firstImage) {
-    port.postMessage({ msg: 'Error' });
+  try {
+    const firstImage = waitForElement('img[fetchpriority="high"]');
+
+    if (firstImage.computedStyleMap().get('cursor').value !== 'zoom-in') {
+      port.postMessage({
+        msg: 'DownloadThis',
+        data: {
+          url: firstImage.src,
+          filename:
+            urlToImageName(window.location.href) +
+            '.' +
+            urlToExtension(firstImage.src),
+          msg: 'Preview Image',
+        },
+      });
+      return;
+    }
+    firstImage.click();
+  } catch(ex) {
+    port.postMessage({ msg: 'Error', data: 'Could not find preview image' });
     return;
   }
-  if (firstImage.computedStyleMap().get('cursor').value !== 'zoom-in') {
-    port.postMessage({
-      msg: 'DownloadThis',
-      data: {
-        url: firstImage.src,
-        filename:
-          urlToImageName(window.location.href) +
-          '.' +
-          urlToExtension(firstImage.src),
-        msg: 'Preview Image',
-      },
-    });
-    return;
-  }
-  firstImage.click();
+
   setTimeout(() => {
     const image = document.querySelector('.ReactModalPortal img');
     if (!image?.src) {
-      port.postMessage({ msg: 'Error' });
+      port.postMessage({ msg: 'Error', data: 'Could not find modal image' });
       return;
     }
     image.style.outline = '5px solid purple';
@@ -72,7 +90,76 @@ function openModalFallback() {
       },
     });
   }, MODAL_WAIT_MS);
-}
+
+})();
+
+// const search = setInterval(() => {
+  
+
+//   if (downloadButton?.href) {
+//     clearInterval(search);
+//     downloadButton.style.outline = '5px solid purple';
+//     port.postMessage({
+//       msg: 'DownloadThis',
+//       data: {
+//         url: downloadButton.href,
+//         filename:
+//           urlToImageName(window.location.href) +
+//           '.' +
+//           urlToExtension(downloadButton.href),
+//         msg: 'Download Button',
+//       },
+//     });
+//     return;
+//   }
+
+//   if (Date.now() - started > DOWNLOAD_SEARCH_MS) {
+//     clearInterval(search);
+//     openModalFallback();
+//   }
+// }, INTERVAL_MS);
+
+// function openModalFallback() {
+//   const firstImage = document.querySelector('img[fetchpriority="high"]');
+//   if (!firstImage) {
+//     port.postMessage({ msg: 'Error' });
+//     return;
+//   }
+//   if (firstImage.computedStyleMap().get('cursor').value !== 'zoom-in') {
+//     port.postMessage({
+//       msg: 'DownloadThis',
+//       data: {
+//         url: firstImage.src,
+//         filename:
+//           urlToImageName(window.location.href) +
+//           '.' +
+//           urlToExtension(firstImage.src),
+//         msg: 'Preview Image',
+//       },
+//     });
+//     return;
+//   }
+//   firstImage.click();
+//   setTimeout(() => {
+//     const image = document.querySelector('.ReactModalPortal img');
+//     if (!image?.src) {
+//       port.postMessage({ msg: 'Error' });
+//       return;
+//     }
+//     image.style.outline = '5px solid purple';
+//     port.postMessage({
+//       msg: 'DownloadThis',
+//       data: {
+//         url: image.src,
+//         filename:
+//           urlToImageName(window.location.href) +
+//           '.' +
+//           urlToExtension(image.src),
+//         msg: 'Preview Image',
+//       },
+//     });
+//   }, MODAL_WAIT_MS);
+// }
 
 function urlToImageName(url) {
   const i = url.lastIndexOf('/');
